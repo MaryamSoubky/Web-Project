d3d27bc10b88e7f354902b8886096e1bb26441ef
Minahatecoding <151873386+Minahatecoding@users.noreply.github.com>
2024-06-21 14:24:08 +0300

    updated contact us page
    added 6 images for blog page and external api's for contact us page

--- public/js/Contact page.js
+++ public/js/Contact page.js
@@ -1,91 +1,88 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const FN = document.getElementById('FN');
-const LN = document.getElementById('LN');
+const form = document.querySelector("form");
+  const fullName = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const subject = document.getElementById("subject");
+  const mess = document.getElementById("message");
 
-
-// Show input error message
-function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = 'input-box error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
-}
-
-// Show success outline
-function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'input-box success';
-}
-
-// Check email is valid
-function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
-}
-
-// Check required fields
-function checkRequired(inputArr) {
-  let isRequired = false;
-  inputArr.forEach(function(input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-      isRequired = true;
-    } else {
-      showSuccess(input);
+  function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.
+      value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+    
+    Email.send({
+      SecureToken:"f07bd2e4-a549-4902-a585-c7b119ad5fbf",
+      To: 'booklyy.store@gmail.com',
+      From: "booklyy.store@gmail.com",
+      Subject: subject.value,
+      Body: bodyMessage
+    }).then(
+      message =>{
+        if(message="OK") {
+        Swal.fire({
+          title:"Success",
+          text:"Message Sent Successfuly",
+          icon:"Success"
+        });
+      }
     }
-  });
-
-  return isRequired;
-}
-
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
+  );
   }
-}
+  function checkinputs(){
+    const items=document.querySelectorAll(".item");
 
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
+    for(const item of items){
+      if(item.value==""){ 
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+      if(items[1].value!=""){
+checkemail();
+      }
+      items[1].addEventListener("keyup",()=>{
+        checkemail();
+      });
+      item.addEventListener("keyup",()=>{
+        if(item.value != ""){
+          item.classList.remove("error");
+          item.parentElement.classList.remove("error");
+    }
+    else{
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
   }
-}
-
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-// Event listeners
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  if(checkRequired([FN,LN,email, password])){
-    checkLength(password, 6, 25);
-    checkEmail(email);
-    checkLength(FN, 3, 25);
-    checkLength(LN, 3, 25);
-  }
-  /*if(checkRequired([email, password])){
-    checkLength(password, 6, 25);
-    checkEmail(email);
-  }*/
-
 });
+    }
+  }
+  function checkemail(){
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+    const errortxtemail =document.querySelector(".error-txt.email");
+    if(!email.value.match(emailRegex)){
+email.classList.add("error");
+email.parentElement.classList.add("error");
+    if(email.value!= ""){
+      errortxtemail.innerText="enter a valid address";
+    }
+    else{
+      errortxtemail.innerText="emaill address can't be blank";
+    }
+  }
+    else{
+      email.classList.remove("error");
+      email.parentElement.classList.remove("error");
+    }
+  }
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkinputs();
+
+    if(!fullName.classList.contains("error")&&!email.classList.contains
+    ("error")&&!phone.classList.contains("error")&&!subject.classList
+      .contains("error")&&!mess.classList.contains("error")){
+        sendEmail();
+        
+        form.reset();
+        return false;
+      }
+  });
